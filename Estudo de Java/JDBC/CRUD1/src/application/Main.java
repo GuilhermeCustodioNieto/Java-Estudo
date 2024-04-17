ef524b85213b64132f8387aeac4e52628166fd86
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-04-17 15:34:39 -0300

    First select on JDBC
    The first select on JDBC created

--- Estudo de Java/JDBC/CRUD1/src/application/Main.java
+++ Estudo de Java/JDBC/CRUD1/src/application/Main.java
@@ -1,12 +1,34 @@
 package application;
 
+import com.mysql.cj.xdevapi.DbDoc;
 import db.DB;
+import db.DbException;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try{
+            conn = DB.getConnection();
+
+            st = conn.createStatement();
+
+            rs = st.executeQuery("SELECT * FROM department;");
+
+            while(rs.next()){
+                System.out.println(rs.getInt("Id") + " " + rs.getString("Name"));
+
+            }
+
+        }catch(SQLException e){
+            throw new DbException(e.getMessage());
+        }
     }
 }
