d7c7c7ba630db10e6503a452dc22b69abb6845f4
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-04-17 21:20:20 -0300

    INSERT INTO on Java using JDBC
    Created INSERT INTO on Java using Java and JDBC

--- Estudo de Java/JDBC/CRUD1/src/application/Main.java
+++ Estudo de Java/JDBC/CRUD1/src/application/Main.java
@@ -4,35 +4,47 @@ import com.mysql.cj.xdevapi.DbDoc;
 import db.DB;
 import db.DbException;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Main {
     public static void main(String[] args) {
+        SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
+
         Connection conn = null;
-        Statement st = null;
-        ResultSet rs = null;
+        PreparedStatement st = null;
 
         try{
             conn = DB.getConnection();
 
-            st = conn.createStatement();
+            st = conn.prepareStatement("INSERT INTO seller " +
+                    "(Name, Email, BirthDate, BaseSalary, DepartmentId) VALUES "
+                    + "(?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
 
-            rs = st.executeQuery("SELECT * FROM department;");
+            st.setString(1,"Guilherme Nieto");
+            st.setString(2, "guilhermecustodionieto@gmail.com");
+            st.setDate(3, new Date(fmt.parse("11/09/2001").getTime()));
+            st.setDouble(4, 3000.0);
+            st.setInt(5, 4);
 
-            while(rs.next()){
-                System.out.println(rs.getInt("Id") + " " + rs.getString("Name"));
+            int rowsUpdated = st.executeUpdate();
 
+            if(rowsUpdated > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+
+                while(rs.next()){
+                    int id = rs.getInt(1);
+                    System.out.println("Done! Id = " + id);
+                }
             }
 
         }catch(SQLException e){
             throw new DbException(e.getMessage());
-        }
-
-        finally{
-            DB.closeResultSet(rs);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        } finally{
             DB.closeStatment(st);
             DB.closeConnection();
         }
