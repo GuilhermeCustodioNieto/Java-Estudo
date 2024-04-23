2fe19f897e9002e42a577744373826375b1c5e7d
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-04-23 17:14:37 -0300

    Implemented transactions on JDBC
    Implemented transactions on JDBC, using the rollBack, commit, and setAutoCommit functions.

--- Estudo de Java/JDBC/CRUD1/src/application/Main.java
+++ Estudo de Java/JDBC/CRUD1/src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import com.mysql.cj.xdevapi.DbDoc;
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 
 import java.sql.*;
 import java.text.ParseException;
@@ -10,40 +11,36 @@ import java.text.SimpleDateFormat;
 
 public class Main {
     public static void main(String[] args) {
-        SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
+
+        Oi oi = new Oi();
 
         Connection conn = null;
-        PreparedStatement st = null;
+        Statement st = null;
 
         try{
             conn = DB.getConnection();
 
-            st = conn.prepareStatement("INSERT INTO seller " +
-                    "(Name, Email, BirthDate, BaseSalary, DepartmentId) VALUES "
-                    + "(?, ?, ?, ?, ?)",
-                    Statement.RETURN_GENERATED_KEYS);
+            conn.setAutoCommit(false);
 
-            st.setString(1,"Guilherme Nieto");
-            st.setString(2, "guilhermecustodionieto@gmail.com");
-            st.setDate(3, new Date(fmt.parse("11/09/2001").getTime()));
-            st.setDouble(4, 3000.0);
-            st.setInt(5, 4);
+            st = conn.createStatement();
 
-            int rowsUpdated = st.executeUpdate();
+            int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+            int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
 
-            if(rowsUpdated > 0) {
-                ResultSet rs = st.getGeneratedKeys();
+            conn.commit();
 
-                while(rs.next()){
-                    int id = rs.getInt(1);
-                    System.out.println("Done! Id = " + id);
-                }
-            }
+            System.out.println("Rows 1 " + rows1);
+            System.out.println("Rows 2 " + rows2);
 
         }catch(SQLException e){
-            throw new DbException(e.getMessage());
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            try {
+                conn.rollback();
+                throw new DbException("Transaction rolled back! Caused by: " + e.getMessage());
+            } catch (SQLException ex) {
+
+                throw new DbException("Error trying to rollback: " + ex.getMessage());
+            }
         } finally{
             DB.closeStatment(st);
             DB.closeConnection();
