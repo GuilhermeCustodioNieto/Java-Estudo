81839560bf7c7665b9d6b25c8a222eab6405fa7f
Guilherme Custodio Nieto <GuilhermeCustodioNieto@gmail.com>
2024-04-24 08:36:27 -0300

    Interfaces SellerDao and DepartmentDao addes
    The interfaces has been added

---
+++ Estudo de Java/JDBC/EmployeeManagemet/src/model/dao/SellerDao.java
@@ -0,0 +1,11 @@
+package model.dao;
+
+import java.util.List;
+
+public interface SellerDao {
+    void insert (Seller obj);
+    void  update (Seller obj);
+    void  deleteById (Seller obj);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}
