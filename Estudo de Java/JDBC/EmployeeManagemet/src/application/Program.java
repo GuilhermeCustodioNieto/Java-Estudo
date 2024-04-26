40a40e34faea12fb51c6b300a15a2a842af589e7
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-04-26 20:15:59 -0300

    Created SellerDaoJDBC and DaoFactory on Employee Management project

--- Estudo de Java/JDBC/EmployeeManagemet/src/application/Program.java
+++ Estudo de Java/JDBC/EmployeeManagemet/src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(obj);
     }
