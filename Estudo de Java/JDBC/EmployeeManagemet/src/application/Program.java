a82c12d6382d0b29cef4ef75071032bb35a34ef1
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-04-26 22:03:52 -0300

    Employee Management - Insert function implemented

--- Estudo de Java/JDBC/EmployeeManagemet/src/application/Program.java
+++ Estudo de Java/JDBC/EmployeeManagemet/src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -25,10 +26,15 @@ public class Program {
         }
 
 
-        System.out.println("\n===TEST 1: seller findAll");
+        System.out.println("\n===TEST 3: seller findAll");
         list = sellerDao.findAll();
         for(Seller obj : list){
             System.out.println(obj.toString());
         }
+
+        System.out.println("\n===TEST 4: seller insert");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Insered! New id = " + newSeller.getId());
     }
 }
