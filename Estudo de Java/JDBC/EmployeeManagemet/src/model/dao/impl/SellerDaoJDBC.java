a45ca086bb70c8d1ecdbb74b8b9cf96a25c72b2a
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-04-26 21:43:13 -0300

    Employee Management - findByDepartment implemented
    findByDepartment has been implemented.

--- Estudo de Java/JDBC/EmployeeManagemet/src/model/dao/impl/SellerDaoJDBC.java
+++ Estudo de Java/JDBC/EmployeeManagemet/src/model/dao/impl/SellerDaoJDBC.java
@@ -1,6 +1,7 @@
 package model.dao.impl;
 
 import db.DbException;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -9,7 +10,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SellerDaoJDBC implements SellerDao {
 
@@ -87,5 +91,44 @@ public class SellerDaoJDBC implements SellerDao {
     @Override
     public List<Seller> findAll() {
         return null;
+    }
+
+    @Override
+    public List<Seller> findByDepartment(Department department) {
+
+        PreparedStatement st = null;
+
+        ResultSet rs = null;
+
+        try{
+            st = conn.prepareStatement( "SELECT seller.*,department.Name as DepName\n" +
+                    "FROM seller INNER JOIN department\n" +
+                    "ON seller.DepartmentId = department.Id\n" +
+                    "WHERE DepartmentId = ?\n" +
+                    "ORDER BY Name");
+
+            st.setInt(1, department.getId());
+            rs = st.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map= new HashMap<>();
+            while (rs.next()){
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                if(dep == null) {
+                    dep = instantDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller obj = instantSellet(rs, dep);
+                list.add(obj);
+            }
+
+
+            return list;
+        } catch(SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+
     }
 }
