6ca7f4276eb23fade485974587c0774a8b349a3b
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-04-26 21:51:34 -0300

    Employee Management - findAll method implemented
    The findAll method (from the SellerDaoJDBC class) has been created and added.

--- Estudo de Java/JDBC/EmployeeManagemet/src/application/Program.java
+++ Estudo de Java/JDBC/EmployeeManagemet/src/application/Program.java
@@ -17,9 +17,16 @@ public class Program {
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
-        System.out.println("\n===TEST 1: seller findById");
+        System.out.println("\n===TEST 2: seller findByDepartment");
         Department department =new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
+        for(Seller obj : list){
+            System.out.println(obj.toString());
+        }
+
+
+        System.out.println("\n===TEST 1: seller findAll");
+        list = sellerDao.findAll();
         for(Seller obj : list){
             System.out.println(obj.toString());
         }
--- Estudo de Java/JDBC/EmployeeManagemet/src/model/dao/impl/SellerDaoJDBC.java
+++ Estudo de Java/JDBC/EmployeeManagemet/src/model/dao/impl/SellerDaoJDBC.java
@@ -90,7 +90,38 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public List<Seller> findAll() {
-        return null;
+
+        PreparedStatement st = null;
+
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement("SELECT seller.*,department.Name as DepName\n" +
+                    "FROM seller INNER JOIN department\n" +
+                    "ON seller.DepartmentId = department.Id\n" +
+                    "ORDER BY Name");
+
+            rs = st.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+            while (rs.next()) {
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                if (dep == null) {
+                    dep = instantDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller obj = instantSellet(rs, dep);
+                list.add(obj);
+            }
+
+
+            return list;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
     }
 
     @Override
