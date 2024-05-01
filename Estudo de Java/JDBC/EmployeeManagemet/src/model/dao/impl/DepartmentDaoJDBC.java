57f7fc392cebca9d9fab2dd719a8e05f657bdb34
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-04-30 23:41:44 -0300

    Employee Management - update, deleteById and findAll methods from DepartmentDaoJDBC
    Other methods have been added to the DepartmentDaoJDBC class, they are:
    - update
    - deleteById
    - findAll

--- Estudo de Java/JDBC/EmployeeManagemet/src/model/dao/impl/DepartmentDaoJDBC.java
+++ Estudo de Java/JDBC/EmployeeManagemet/src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,10 +1,12 @@
 package model.dao.impl;
 
+import db.DB;
 import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
@@ -22,11 +24,31 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public void update(Department obj) {
+        try {
+            PreparedStatement st = conn.prepareStatement("UPDATE department \n" +
+                    "SET Id = ?, Name = ?\n" +
+                    "WHERE Id = ?");
 
+            st.setInt(1, obj.getId());
+            st.setString(2, obj.getName());
+
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
     }
-
     @Override
     public void deleteById(Department obj) {
+        try {
+            PreparedStatement st = conn.prepareStatement("DELETE FROM department WHERE Id = ?");
+
+            st.setInt(1, obj.getId());
+
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+
 
     }
 
@@ -53,6 +75,22 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public List<Department> findAll() {
-        return null;
+        List<Department> departments = new ArrayList<>();
+
+        try{
+            PreparedStatement st = conn.prepareStatement("SELECT * FROM department");
+
+            ResultSet rs = st.executeQuery();
+
+            while(rs.next()) {
+                departments.add(new Department(rs.getInt("Id"), rs.getString("Name")));
+            }
+
+            return departments;
+
+
+        }catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
     }
 }
