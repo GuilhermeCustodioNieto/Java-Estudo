24e76cff6ac15c22d17d52e73bbc8e5acab9bb71
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-05-04 23:50:07 -0300

    Functional Programming - Lambda Exrpession declared to Function interface

--- Estudo de Java/FunctionalProgramming/LambdaExpressions/src/com/application/Main.java
+++ Estudo de Java/FunctionalProgramming/LambdaExpressions/src/com/application/Main.java
@@ -5,6 +5,7 @@ import com.util.UpperCaseName;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -16,7 +17,9 @@ public class Main {
         list.add(new Product("Tablet", 350.0));
         list.add(new Product("HD Case", 80.90));
 
-        List<String> names =  list.stream().map(Product::nonStaticUpperCaseName ).collect(Collectors.toList());
+        Function<Product, String> func = p -> p.getName().toUpperCase();
+
+        List<String> names =  list.stream().map(func).collect(Collectors.toList());
 
         names.forEach(System.out::println);
     }
