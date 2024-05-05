1bf2221c4c069f0965794b3a25dda6cc6559b1c1
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-05-04 23:28:12 -0300

    Function Programming - nonStaticPriceUpdate consumer

--- Estudo de Java/FunctionalProgramming/LambdaExpressions/src/com/application/Main.java
+++ Estudo de Java/FunctionalProgramming/LambdaExpressions/src/com/application/Main.java
@@ -15,7 +15,7 @@ public class Main {
         list.add(new Product("Tablet", 350.0));
         list.add(new Product("HD Case", 80.90));
 
-        list.forEach(Product::staticPriceUpdate);
+        list.forEach(Product::nonStaticPriceUpdate);
 
         list.forEach(System.out::println);
     }
