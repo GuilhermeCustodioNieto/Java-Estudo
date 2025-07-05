95d47f832e35f6c0d2754b7631a50ca09fd6b647
Guilherme Custodio Nieto <GuilhermeCustodioNieto@gmail.com>
2025-07-05 19:33:50 -0300

    feat: created the libary api

---
+++ Estudo de Spring/Udemy/03 - JPA/libraryapi/src/main/java/com/github/guilhermecustodionieto/libraryapi/LibraryapiApplication.java
@@ -0,0 +1,13 @@
+package com.github.guilhermecustodionieto.libraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryapiApplication.class, args);
+	}
+
+}
---
+++ Estudo de Spring/Udemy/03 - JPA/libraryapi/src/test/java/com/github/guilhermecustodionieto/libraryapi/LibraryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.guilhermecustodionieto.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
